883b4ec3edae99334515e729a54e2ccb8ebbc14d
Eugene Soshyn <sichi331@gmail.com>
2024-09-28 16:33:15 +0300

    add main components. add styles and add main logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [feedback, setFeedback] = useState(() => {
+    const savedGood = window.localStorage.getItem("good");
+    const savedNeutral = window.localStorage.getItem("neutral");
+    const savedBad = window.localStorage.getItem("bad");
 
+    return {
+      good: savedGood ? parseInt(savedGood) : 0,
+      neutral: savedNeutral ? parseInt(savedNeutral) : 0,
+      bad: savedBad ? parseInt(savedBad) : 0,
+    };
+  });
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedbackType) => {
+      switch (feedbackType) {
+        case "good":
+          return {
+            ...prevFeedbackType,
+            good: prevFeedbackType.good + 1,
+          };
+        case "neutral":
+          return {
+            ...prevFeedbackType,
+            neutral: prevFeedbackType.neutral + 1,
+          };
+        case "bad":
+          return {
+            ...prevFeedbackType,
+            bad: prevFeedbackType.bad + 1,
+          };
+        case "reset":
+          return {
+            ...prevFeedbackType,
+            good: 0,
+            neutral: 0,
+            bad: 0,
+          };
+        default:
+          return { prevFeedbackType };
+      }
+    });
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem("good", feedback.good);
+    window.localStorage.setItem("neutral", feedback.neutral);
+    window.localStorage.setItem("bad", feedback.bad);
+  }, [feedback]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Description />
+      <Options setFeedback={updateFeedback} totalFeedback={totalFeedback} />
+      {totalFeedback === 0 ? (
+        <Notification />
+      ) : (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positive={positiveFeedback}
+        />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
